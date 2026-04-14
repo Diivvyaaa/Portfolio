d892adfba7d218fe95696abee17cba3ba8286506
Divya <divyaakalia@gmail.com>
2026-04-14 16:06:59 +0530

    updated firebase

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBsdoN0nbN4HOphTHGg4yeXqUexyyxBuFk",
-  authDomain: "my-portfolio-265ea.firebaseapp.com",
-  projectId: "my-portfolio-265ea",
-  storageBucket: "my-portfolio-265ea.firebasestorage.app",
-  messagingSenderId: "78860462288",
-  appId: "1:78860462288:web:c9aa8ac7a1cf1e462856d5"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
